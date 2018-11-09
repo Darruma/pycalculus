9fb9321edf44be233ab99b5d558342eb7dd43cbc
Darruma <umerderr@gmail.com>
2018-11-09 11:26:55 +0000

    fixed syntax errors

--- pycalculus.py
+++ pycalculus.py
@@ -30,14 +30,11 @@ def integrate(f): # takes a function f
 choice = input('Type I for integration and D for differentiation')
 func = input("enter function")
 
-if(choice=='I')
-{
+if(choice=='I'):
     lower = input('input lower bound for integral')
     higher = input('input higher bound for integral')
     print(integrate(eval(func))(lower,higher))
-}
-if(choice == 'D')
-{
-    point = input('input the point at which you want to find the derivative');
+
+if(choice == 'D'):
+    point = int(input('input the point at which you want to find the derivative'))
     print(derivative_operator(eval(func))(point))
-}

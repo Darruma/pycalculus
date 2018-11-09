8524ffc874315f18f8f262b7a26fc2680d1410c8
Darruma <umerderr@gmail.com>
2018-11-09 14:37:30 +0000

    added input mechanism using eval

---
+++ check_topology.py
@@ -0,0 +1,14 @@
+import Topological_Space from topological_space
+main = input('Enter the set for your topological space')
+topology = input('Enter your topology');
+
+T = Topological_Space(eval(main),eval(topology));
+
+if(T.axiom1):
+    print("axiom 1 is satisfied")
+
+if(T.axiom2):
+    print("axiom 2 is satisfied");
+
+if(T.axiom3):
+    print("axiom 3 is satisfied")

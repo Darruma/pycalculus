8b25961e91ed1a9919ac8ab048aff944424f5046
Darruma <umerderr@gmail.com>
2018-11-27 12:07:39 +0000

    started writing calculus parser

--- pycalculus.py
+++ pycalculus.py
@@ -1,6 +1,7 @@
 import math
 from calculus import Calculus
 from func import Function
+from functools import reduce
 storedFunctions = {}
 calc = Calculus(6)
 while 1:
@@ -9,7 +10,10 @@ while 1:
 	if not equalsPosition == -1:
 		name = user_input[0:equalsPosition].strip(" ")
 		function = Function(user_input[equalsPosition:])
-		storedFunctions[name] = function
+		storedFunctions.update({name:function})
+	elif user_input[:3] == 'add':
+		plusFunctions = user_input[3:].split(" ")[1:]
+		
 	elif user_input == 'functions':
 		for function in list(storedFunctions.keys()):
 			print(function + " := " + storedFunctions[function].func)

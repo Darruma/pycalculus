eaf1223433a90f16d3d4d68dfb491a791b228f73
Darruma <umerderr@gmail.com>
2018-11-27 12:13:41 +0000

    added description of evaluator;

--- pycalculus.py
+++ pycalculus.py
@@ -5,15 +5,19 @@ from functools import reduce
 storedFunctions = {}
 calc = Calculus(6)
 while 1:
-	user_input = input(">> ")
+	user_input = input(">> ").split(" ")
 	equalsPosition = user_input.find('=')
 	if not equalsPosition == -1:
-		name = user_input[0:equalsPosition].strip(" ")
-		function = Function(user_input[equalsPosition:])
-		storedFunctions.update({name:function})
+		identifier = user_input[0:equalsPosition]
+		expression = Function(user_input[equalsPosition:])
+		# if the expressions is a literal function , add it to the dictionary,
+		# e.g f(x) = sin(3x) or g(x) = cos(2x)
+		# else, evaluate the expression then add it to the dictionary
+		# e.g g(x) = f(x)+g(x)
+		storedFunctions.update({name:expression})
 	elif user_input[:3] == 'add':
-		plusFunctions = user_input[3:].split(" ")[1:]
 		
+
 	elif user_input == 'functions':
 		for function in list(storedFunctions.keys()):
 			print(function + " := " + storedFunctions[function].func)

8b70bd9f758d34ebd1cad54124dbe2e067814756
Darruma <umerderr@gmail.com>
2018-11-29 13:48:16 +0000

    can differentiate with two functions operated on each other

--- pycalculus.py
+++ pycalculus.py
@@ -4,47 +4,48 @@ from func import Function
 from functools import reduce
 storedFunctions = {}
 calc = Calculus(6)
+
+def parseFunctionExpression(declaration):
+	print(declaration)
+	if len(declaration) == 1:
+		if declaration[0] in storedFunctions.keys():
+			return storedFunctions[declaration[0]]
+		return Function(declaration[0])
+	else:
+		operator = declaration[1]
+		if declaration[0] in list(storedFunctions.keys()):
+			if declaration[2] in list(storedFunctions.keys()):
+				leftFunc = storedFunctions[declaration[0]]
+				rightFunc = storedFunctions[declaration[2]]
+				if operator == '+':
+					return leftFunc + rightFunc
+				elif operator == '*':
+					return leftFunc * rightFunc
+				elif operator == '-':
+					return leftFunc - rightFunc
+				elif operator == '.':
+					return leftFunc.compose(rightFunc)
+			else:
+				print(">> " +  declaration[2] + " not found")
+		else:
+			print(">> "  + declaration[0] + " not found")
+	return None
+
 while 1:
 	user_input = input(">> ").split(" ")
 	if user_input[0] == 'differentiate':
-		func = user_input[1]
-		val = user_input[len(user_input) -1]
-		if func in list(storedFunctions.keys()):
-			function = 'lambda x:' + storedFunctions[func].func
-			val = float(val)
-			print(">> " +calc.derivative_operator(eval(function))(val))
-		else:
-			print(">> Error entering function")
+		diffble_input = user_input[1:user_input.index('at')]
+		diffblefunc = parseFunctionExpression(diffble_input)
+		if diffblefunc is not None:
+			val = float(user_input[len(user_input)-1])
+			derivative_at_val = str(calc.derivative_operator(eval('lambda x:' + diffblefunc.func))(val))
+			print(">> " + derivative_at_val)
 	elif "=" in user_input: 
 		equals_index = user_input.index("=")
 		identifier = user_input[equals_index - 1]
-		declaration = user_input[equals_index+1:]
-		if len(declaration) == 1:
-			storedFunctions.update({identifier:Function(declaration[0])});
-			print(">> Stored function with name " + identifier)
-		else:
-			operator = declaration[1]
-			if declaration[0] in list(storedFunctions.keys()):
-				if declaration[2] in list(storedFunctions.keys()):
-					leftFunc = storedFunctions[declaration[0]]
-					rightFunc = storedFunctions[declaration[2]]
-					if operator == '+':
-						storedFunctions.update({identifier:leftFunc + rightFunc})
-					elif operator == '*':
-						storedFunctions.update({identifier:leftFunc * rightFunc})
-					elif operator == '-':
-						storedFunctions.update({identifier:leftFunc - rightFunc})
-					elif operator == '.':
-						storedFunctions.update({identifier:leftFunc.compose(rightFunc)})
-				else:
-					print(">> " +  declaration[2] + " not found")
-			else:
-				print(">> "  + declaration[0] + " not found")
-
-			
-
-
-			# perform operations on functions
+		func = parseFunctionExpression(user_input[equals_index+1:])
+		if func is not None:
+			storedFunctions.update({identifier:func})
 	elif 'functions' in user_input:
 		for function in list(storedFunctions.keys()):
 			print(">> " + function + ":= " + storedFunctions[function].readable_func)
